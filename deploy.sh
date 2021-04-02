87a53edf107a19f9bef21b44af0330bec3d3b785
mkaiserincomm <mkaiser@incomm.com>
2021-04-02 13:45:46 +0000

    Test Change

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ docker build -t mgkaiser/mssqltest1:latest .
 docker push mgkaiser/mssqltest1:latest
 helm upgrade --install -f ./charts/mssqltest1/values.yaml mssqltest1 ./charts/mssqltest1 --namespace incomm-poc 
 for i in $(docker images --filter=dangling=true --format "{{.ID}}"); do docker rm $i; done
+
